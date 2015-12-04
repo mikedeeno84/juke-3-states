38ceb14612b73a88d77016ee58fc34029e89ff7c
Michael DeNatale <mikedeeno84@gmail.com>
2015-12-04 15:27:13 -0500

    added states for all views

--- browser/js/controllers/album.js
+++ browser/js/controllers/album.js
@@ -1,4 +1,4 @@
-app.controller('AlbumCtrl', function ($scope, $rootScope, PlayerFactory, AlbumFactory) {
+app.controller('AlbumCtrl', function ($scope, $rootScope, PlayerFactory, AlbumFactory, $stateParams) {
 
 	$scope.isCurrent = function (song) {
 		var current = PlayerFactory.getCurrentSong();
@@ -8,16 +8,26 @@ app.controller('AlbumCtrl', function ($scope, $rootScope, PlayerFactory, AlbumFa
 		PlayerFactory.start(song, $scope.album.songs);
 	};
 
-	$rootScope.$on('changeView', function (evt, data) {
-		if (data.name == 'oneAlbum') {
-			$scope.showMe = true;
-			AlbumFactory.fetchById(data.id)
-			.then(function (album) {
-				$scope.album = album;
-			});
-		} else {
-			$scope.showMe = false;
-		}
-	});
 
+
+
+
+	// $rootScope.$on('changeView', function (evt, data) {
+	// 	if (data.name == 'oneAlbum') {
+	// 		$scope.showMe = true;
+	AlbumFactory.fetchById($stateParams.albumId) //hard coded album ID
+	.then(function (album) {
+		$scope.album = album;
+	});
+	// 	} else {
+	// 		$scope.showMe = false;
+	// 	}
+	// });
+
+}).config(function($stateProvider){
+	$stateProvider.state('album',{
+		url: '/albums/:albumId',
+		templateUrl: '/views/album.html',
+		controller:'AlbumCtrl'
+	});
 });
